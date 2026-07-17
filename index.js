47fd00d6fcfb1ab5a890aabd25e2734fbd29fb21
Tarun Kumar <tarun.25b01010853@abes.ec.in>
2026-07-17 10:08:08 +0530

    Day-1

---
+++ index.js
@@ -0,0 +1,12 @@
+function lineofstars(){
+    let ans = "";
+    for(let i=0 ;i<5; i++){
+        ans += "* ";
+    }
+    console.log(ans);
+}
+lineofstars();
+console.log("Hello ");
+lineofstars();
+console.log("World");
+lineofstars();
